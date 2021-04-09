c956f9863751165d19fd3eb9260c153fa0c22ba2
Ann Impalovskaya <covertover@gmail.com>
2021-04-09 14:09:47 +0300

    tapped cards

--- lib/main.dart
+++ lib/main.dart
@@ -11,44 +11,54 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  static final Color color1 = const Color(0xfff4f1de);
-  static final Color color2 = const Color(0xff0b090a);
-  static final Color color3 = const Color(0xffba181b);
+  static final Color colorWhite = const Color(0xfff4f1de);
+  static final Color colorBlack = const Color(0xff0b090a);
+  static final Color colorRed = const Color(0xffba181b);
+  static final Color colorGray = const Color(0xff212529);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        primaryColor: color2,
-        scaffoldBackgroundColor: color2,
+        primaryColor: colorBlack,
+        scaffoldBackgroundColor: colorBlack,
         fontFamily: 'Fira Sans',
         brightness: Brightness.dark,
         textTheme: TextTheme(
-          button: TextStyle(color: color1),
-          bodyText1: TextStyle(color: color1),
-          bodyText2: TextStyle(color: color1),
+          button: TextStyle(color: colorWhite),
+          bodyText1: TextStyle(color: colorWhite),
+          bodyText2: TextStyle(color: colorWhite),
         ),
       ),
       title: 'Let\'s listen to music!',
       home: Scaffold(
         appBar: AppBar(
-          title: Card(
-            color: color2,
-            margin: EdgeInsets.only(left: 15.0),
-            //  <Widget> [
-            child: ListTile(
-              leading: Icon(
-                Icons.music_note,
-                color: color3,
-              ),
-              title: Text(
-                'Music',
-                style: TextStyle(
-                    fontSize: 25.0, color: color1, fontFamily: 'League'),
-              ),
-            ),
-            // ],
+          leading: Icon(
+            Icons.play_circle_outline,
+            color: colorRed,
           ),
+          title: Text(
+            'Music',
+            style: TextStyle(
+              fontSize: 30.0,
+              color: colorWhite,
+              fontFamily: 'League',
+            ),
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search, color: colorWhite),
+              onPressed: () {
+                print('Searching ...');
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.music_video, color: colorWhite),
+              onPressed: () {
+                print('Enjoy watching!');
+              },
+            ),
+          ],
         ),
         body: SingleChildScrollView(
           padding: EdgeInsets.only(bottom: 5.0),
@@ -57,8 +67,8 @@ class _MyAppState extends State<MyApp> {
               Text(
                 'Choose your mood',
                 style: TextStyle(
-                  color: color1,
-                  fontSize: 15.0,
+                  color: colorWhite,
+                  fontSize: 17.0,
                 ),
                 textAlign: TextAlign.left,
               ),
@@ -70,53 +80,61 @@ class _MyAppState extends State<MyApp> {
                     Container(
                       padding: EdgeInsets.only(right: 10.0),
                       child: OutlineButton(
-                        onPressed: null,
+                        onPressed: () {
+                          print('Tapped!');
+                        },
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(28.0),
                         ),
                         child: Text(
                           'Workout',
-                          style: TextStyle(color: color1),
+                          style: TextStyle(color: colorWhite),
                         ),
                       ),
                     ),
                     Container(
                       padding: EdgeInsets.only(right: 10.0),
                       child: OutlineButton(
-                        onPressed: null,
+                        onPressed: () {
+                          print('Tapped!');
+                        },
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(28.0),
                         ),
                         padding: EdgeInsets.all(10.0),
                         child: Text(
                           'Relax',
-                          style: TextStyle(color: color1),
+                          style: TextStyle(color: colorWhite),
                         ),
                       ),
                     ),
                     Container(
                       padding: EdgeInsets.only(right: 10.0),
                       child: OutlineButton(
-                        onPressed: null,
+                        onPressed: () {
+                          print('Tapped!');
+                        },
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(28.0),
                         ),
                         child: Text(
                           'Party',
-                          style: TextStyle(color: color1),
+                          style: TextStyle(color: colorWhite),
                         ),
                       ),
                     ),
                     Container(
                       padding: EdgeInsets.only(right: 10.0),
                       child: OutlineButton(
-                        onPressed: null,
+                        onPressed: () {
+                          print('Tapped!');
+                        },
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(28.0),
                         ),
                         child: Text(
                           'Romance',
-                          style: TextStyle(color: color1),
+                          style: TextStyle(color: colorWhite),
                         ),
                       ),
                     ),
@@ -126,12 +144,13 @@ class _MyAppState extends State<MyApp> {
               Align(
                 alignment: Alignment.centerLeft,
                 child: Container(
-                  margin: EdgeInsets.only(left: 15.0),
+                  margin: EdgeInsets.only(left: 15.0, bottom: 10.0),
                   child: Text(
                     'Your favorites',
                     style: TextStyle(
-                        color: color1, fontSize: 25.0, fontFamily: 'League'),
-                    textAlign: TextAlign.left,
+                        color: colorWhite,
+                        fontSize: 25.0,
+                        fontFamily: 'League'),
                   ),
                 ),
               ),
@@ -141,20 +160,40 @@ class _MyAppState extends State<MyApp> {
                   children: [
                     Card(
                       margin: EdgeInsets.only(left: 10.0, right: 10.0),
-                      color: color2,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(8.0),
-                        child: Image.asset(
-                          'assets/images/cover1.jpg',
-                          fit: BoxFit.cover,
-                          width: 200.0,
-                          height: 200.0,
-                        ),
+                      color: colorBlack,
+                      child: Column(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(8.0),
+                            child: Image.asset(
+                              'assets/images/cover1.jpg',
+                              fit: BoxFit.cover,
+                              width: 150.0,
+                              height: 150.0,
+                            ),
+                          ),
+                          Container(
+                            child: Text(
+                              'I wanna be yours',
+                              style:
+                                  TextStyle(color: colorWhite, fontSize: 15.0),
+                              textAlign: TextAlign.left,
+                            ),
+                            margin: EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 5.0),
+                          ),
+                          Container(
+                            child: Text(
+                              'Arctic Monkeys',
+                              style:
+                                  TextStyle(color: colorWhite, fontSize: 10.0),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                     Card(
                       margin: EdgeInsets.only(left: 10.0, right: 10.0),
-                      color: color2,
+                      color: colorBlack,
                       child: ClipRRect(
                         borderRadius: BorderRadius.circular(8.0),
                         child: Image.asset(
@@ -167,7 +206,7 @@ class _MyAppState extends State<MyApp> {
                     ),
                     Card(
                       margin: EdgeInsets.only(left: 10.0, right: 10.0),
-                      color: color2,
+                      color: colorBlack,
                       child: ClipRRect(
                         borderRadius: BorderRadius.circular(8.0),
                         child: Image.asset(
@@ -180,7 +219,7 @@ class _MyAppState extends State<MyApp> {
                     ),
                     Card(
                       margin: EdgeInsets.only(left: 10.0, right: 10.0),
-                      color: color2,
+                      color: colorBlack,
                       child: ClipRRect(
                         borderRadius: BorderRadius.circular(8.0),
                         child: Image.asset(
@@ -197,11 +236,13 @@ class _MyAppState extends State<MyApp> {
               Align(
                 alignment: Alignment.centerLeft,
                 child: Container(
-                  margin: EdgeInsets.only(left: 15.0),
+                  margin: EdgeInsets.only(top: 15.0, left: 15.0, bottom: 10.0),
                   child: Text(
                     'Mixed for you',
                     style: TextStyle(
-                        color: color1, fontSize: 25.0, fontFamily: 'League'),
+                        color: colorWhite,
+                        fontSize: 25.0,
+                        fontFamily: 'League'),
                     textAlign: TextAlign.left,
                   ),
                 ),
@@ -212,7 +253,7 @@ class _MyAppState extends State<MyApp> {
                   children: [
                     Card(
                       margin: EdgeInsets.only(left: 10.0, right: 10.0),
-                      color: color2,
+                      color: colorBlack,
                       child: ClipRRect(
                         borderRadius: BorderRadius.circular(8.0),
                         child: Image.asset(
@@ -225,7 +266,7 @@ class _MyAppState extends State<MyApp> {
                     ),
                     Card(
                       margin: EdgeInsets.only(left: 10.0, right: 10.0),
-                      color: color2,
+                      color: colorBlack,
                       child: ClipRRect(
                         borderRadius: BorderRadius.circular(8.0),
                         child: Image.asset(
@@ -242,11 +283,24 @@ class _MyAppState extends State<MyApp> {
               Align(
                 alignment: Alignment.centerLeft,
                 child: Container(
-                  margin: EdgeInsets.only(left: 15.0),
+                  margin: EdgeInsets.only(top: 15.0, left: 15.0),
                   child: Text(
                     'Listen again',
+                    style: TextStyle(color: colorWhite, fontSize: 15.0),
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Container(
+                  margin: EdgeInsets.only(left: 15.0, bottom: 10.0),
+                  child: Text(
+                    'Your daily music',
                     style: TextStyle(
-                        color: color1, fontSize: 25.0, fontFamily: 'League'),
+                        color: colorWhite,
+                        fontSize: 25.0,
+                        fontFamily: 'League'),
                     textAlign: TextAlign.left,
                   ),
                 ),
@@ -257,7 +311,7 @@ class _MyAppState extends State<MyApp> {
                   children: [
                     Card(
                       margin: EdgeInsets.only(left: 10.0, right: 10.0),
-                      color: color2,
+                      color: colorBlack,
                       child: ClipRRect(
                         borderRadius: BorderRadius.circular(8.0),
                         child: Image.asset(
@@ -270,7 +324,7 @@ class _MyAppState extends State<MyApp> {
                     ),
                     Card(
                       margin: EdgeInsets.only(left: 10.0, right: 10.0),
-                      color: color2,
+                      color: colorBlack,
                       child: ClipRRect(
                         borderRadius: BorderRadius.circular(8.0),
                         child: Image.asset(
@@ -284,8 +338,115 @@ class _MyAppState extends State<MyApp> {
                   ],
                 ),
               ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Container(
+                  margin: EdgeInsets.only(top: 15.0, left: 15.0),
+                  child: Text(
+                    'Start radio based on a song',
+                    style: TextStyle(color: colorWhite, fontSize: 15.0),
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Container(
+                  margin: EdgeInsets.only(left: 15.0, bottom: 10.0),
+                  child: Text(
+                    'Quick picks',
+                    style: TextStyle(
+                        color: colorWhite,
+                        fontSize: 25.0,
+                        fontFamily: 'League'),
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+              ),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Card(
+                    color: colorGray,
+                    child: InkWell(
+                      splashColor: colorBlack,
+                      onTap: () {
+                        print('Card tapped.');
+                      },
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: ListTile(
+                              leading: Image.asset(
+                                'assets/images/cover7.jpg',
+                                fit: BoxFit.cover,
+                                width: 50.0,
+                                height: 50.0,
+                              ),
+                              title: Text('Yamakasi'),
+                              subtitle: Text('Miyagi & Andy Panda'),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Card(
+                    color: colorGray,
+                    child: InkWell(
+                      splashColor: colorBlack,
+                      onTap: () {
+                        print('Card tapped.');
+                      },
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: ListTile(
+                              leading: Image.asset(
+                                'assets/images/cover1.jpg',
+                                fit: BoxFit.cover,
+                                width: 50.0,
+                                height: 50.0,
+                              ),
+                              title: Text('505'),
+                              subtitle: Text('Arctic Monkeys'),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          shape: CircularNotchedRectangle(),
+          child: Row(
+            children: [
+              Spacer(),
+              IconButton(
+                icon: Icon(Icons.home),
+                color: colorWhite,
+                onPressed: () {},
+              ),
+              Spacer(),
+              IconButton(
+                icon: Icon(Icons.explore),
+                color: colorWhite,
+                onPressed: () {},
+              ),
+              Spacer(),
+              IconButton(
+                icon: Icon(Icons.library_music),
+                color: colorWhite,
+                onPressed: () {},
+              ),
+              Spacer(),
+            ],
+          ),
+          color: colorGray,
         ),
       ),
     );
